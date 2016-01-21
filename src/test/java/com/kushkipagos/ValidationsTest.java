9783832426a8c1871350350f693ef3f30b319e01
Luz <luzma_87@yahoo.com>
2016-01-21 16:10:22 -0500

    changing file name from validationsTests to validationTest

--- src/test/java/com/kushkipagos/ValidationsTest.java
+++ src/test/java/com/kushkipagos/ValidationsTest.java
@@ -22,7 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 
 @RunWith(JUnitParamsRunner.class)
-public class ValidationsTests {
+public class ValidationsTest {
     private Kushki kushki;
 
     @Before
