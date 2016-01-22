87e5c9a7e8dd7143963e2f828114510a8d196fed
Luz <luzma_87@yahoo.com>
2016-01-22 11:05:38 -0500

    refactoring tests: creating new file for void integration tests

--- src/test/java/com/kushkipagos/KuskiVoidIntegrationTest_014_019__024.java
+++ src/test/java/com/kushkipagos/KuskiVoidIntegrationTest_014_019__024.java
@@ -15,14 +15,15 @@ import java.security.spec.InvalidKeySpecException;
 import static com.kushkipagos.IntegrationTestsHelpers.*;
 
 /**
- * Created by lmunda on 1/22/16 10:33.
+ * Created by lmunda on 1/22/16 10:50.
  */
-public class KushkiRefundIntegrationTest_009_013__024 {
+public class KuskiVoidIntegrationTest_014_019__024 {
     private Kushki kushki;
     private Transaction tokenTransaction;
     private Transaction chargeTransaction;
-    private Transaction refundTransaction;
+    private Transaction voidTransaction;
     private Double amount;
+    private String ticket;
 
     @Before
     public void setup() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException, KushkiException {
@@ -32,39 +33,36 @@ public class KushkiRefundIntegrationTest_009_013__024 {
         amount = TestsHelpers.getRandomAmount();
         String token = tokenTransaction.getToken();
         chargeTransaction = kushki.charge(token, amount);
-        String ticket = chargeTransaction.getTicketNumber();
+        ticket = chargeTransaction.getTicketNumber();
 
-        refundTransaction = kushki.refundCharge(ticket, amount);
+        voidTransaction = kushki.voidCharge(ticket, amount);
     }
 
     @Test
-    public void shouldReturnSuccessfulRefundTransaction_TC_009() throws BadPaddingException, IllegalBlockSizeException, JsonProcessingException, KushkiException {
+    public void shouldReturnSuccessfulVoidTransaction_TC_014() throws BadPaddingException, IllegalBlockSizeException, JsonProcessingException, KushkiException {
         assertsValidTransaction(tokenTransaction);
         assertsValidTransaction(chargeTransaction);
-        assertsValidTransaction(refundTransaction);
+        assertsValidTransaction(voidTransaction);
     }
 
     @Test
-    public void shouldReturnFailedRefundTransactionNoTicket_TC_012() throws BadPaddingException, IllegalBlockSizeException, JsonProcessingException, KushkiException {
-        Transaction refundTransaction = kushki.refundCharge("", amount);
+    public void shouldReturnFailedVoidTransactionNoTicket_TC_018() throws BadPaddingException, IllegalBlockSizeException, JsonProcessingException, KushkiException {
+        Transaction voidTransaction = kushki.voidCharge("", amount);
 
-        assertsTransaction(refundTransaction, false, "El número de ticket de la transacción es requerido", "705");
+        assertsTransaction(voidTransaction, false, "El número de ticket de la transacción es requerido", "705");
     }
 
     @Test
-    public void shouldReturnFailedRefundTransactionInvalidTicket_TC_013() throws BadPaddingException, IllegalBlockSizeException, JsonProcessingException, KushkiException {
+    public void shouldReturnFailedVoidTransactionInvalidTicket_TC_019() throws BadPaddingException, IllegalBlockSizeException, JsonProcessingException, KushkiException {
         String ticket = "153633977318400068";
 
-        Transaction refundTransaction = kushki.refundCharge(ticket, amount);
+        Transaction voidTransaction = kushki.voidCharge(ticket, amount);
 
-        assertsTransaction(refundTransaction, false, "Transacción no encontrada", "222");
+        assertsTransaction(voidTransaction, false, "Transacción no encontrada", "222");
     }
 
     @Test
     public void shouldReturnFailedRefundTransactionAfterVoidingCharge_TC_024() throws BadPaddingException, IllegalBlockSizeException, JsonProcessingException, KushkiException {
-        String ticket = chargeTransaction.getTicketNumber();
-        Transaction voidTransaction = kushki.voidCharge(ticket, amount);
-
         Transaction refundTransaction = kushki.refundCharge(ticket, amount);
 
         assertsValidTransaction(tokenTransaction);
