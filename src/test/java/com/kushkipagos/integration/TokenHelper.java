b32d582ffcff0c45f4e980b6c29976792a02b90f
Jonathan Reyes <j@jreyes.org>
2016-07-01 14:11:25 -0500

    [KFE-0] Add remember_me field to token API requests

--- src/test/java/com/kushkipagos/integration/TokenHelper.java
+++ src/test/java/com/kushkipagos/integration/TokenHelper.java
@@ -81,6 +81,7 @@ final class TokenHelper {
         parameters.put("currency_code", kushki.getCurrency());
         parameters.put("merchant_identifier", kushki.getMerchantId());
         parameters.put("language_indicator", kushki.getLanguage());
+        parameters.put("remember_me", "0");
         return parameters;
     }
 }
