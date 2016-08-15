16b2c33e817b485fac5e19f898a98cecb4f5b44b
Diego Villacís <diegovillacis101@gmail.com>
2016-08-15 14:20:04 -0500

    +@jreyes33 [KFE-0] Optimize imports

--- src/main/java/com/kushkipagos/Transaction.java
+++ src/main/java/com/kushkipagos/Transaction.java
@@ -1,6 +1,7 @@
 package com.kushkipagos;
 
 import com.fasterxml.jackson.databind.JsonNode;
+
 import javax.ws.rs.core.Response;
 
 public class Transaction {
--- src/test/java/com/kushkipagos/integration/TokenHelper.java
+++ src/test/java/com/kushkipagos/integration/TokenHelper.java
@@ -10,7 +10,11 @@ import com.kushkipagos.Transaction;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
-import javax.ws.rs.client.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
