b0964778023d49434d796a4af21e3b4269d2709a
Roberto Paredes <rdparedessalazar@gmail.com>
2016-09-09 15:41:08 -0500

    [KFE-000] makes curl request to wake up backoffice server

--- pipeline/02-integration.sh
+++ pipeline/02-integration.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+
+# Waking up herokuapp
+curl $BACKOFFICE_URL > /dev/null
+
 gradle integrationtest
 
 # ARTIFACTS:
 # ARTIFACT build/libs
+#
+# ENVIRONMENT VARIABLES:
+#   BACKOFFICE_URL https://backoffice-qa.kushkipagos.com/
