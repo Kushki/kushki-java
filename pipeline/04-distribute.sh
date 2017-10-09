9d438fdcfd11b57ca2ef9c92463242cd44a4f8d4
Victor <vandresmartinez@gmail.com>
2017-10-09 11:27:54 -0500

    KV-2018 thread sleep

--- pipeline/04-distribute.sh
+++ pipeline/04-distribute.sh
@@ -1,14 +1,2 @@
 #!/usr/bin/env bash
-
-#CI_BRANCH
-
-#if [ "$CI_BRANCH" == "DEVELOPE" ]; then
-gradle  clean bintrayUpload
-#fi
-
-# ENVIRONMENT VARIABLES:
-# STAGE
-#  BINTRAY_USER
-#  BINTRAY_API_KEY
-#  MAVEN_CENTRAL_USER
-#  MAVEN_CENTRAL_TOKEN
+gradle clean bintrayUpload
