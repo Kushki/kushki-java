87b0c61c9398a19a5801f4627a08f6275ac546f8
Victor Martinez <vandresmartinez@gmail.com>
2017-10-17 10:43:28 -0500

    update pipeline

--- pipeline/02-integration.sh
+++ pipeline/02-integration.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-# Waking up herokuapp
-curl $BACKOFFICE_URL > /dev/null
-gradle integrationtest
 gradle clean bintrayUpload
-# ARTIFACTS:
-# ARTIFACT build/libs
-#
-# ENVIRONMENT VARIABLES:
-#   BACKOFFICE_URL https://backoffice-qa.kushkipagos.com/
